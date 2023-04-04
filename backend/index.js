7701a610d3bcc6f042334eded8b95b65163cceff
Gabriel Rangel <contato.gabrielrangelgbr@gmail.com>
2023-04-04 11:29:46 -0300

    update backend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3005, () => {
+server.listen(8080, () => {
   console.log('JSON Server is running')
 })
